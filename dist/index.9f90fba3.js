1b1ea00bcfdaf57929d9d91afb7e5dbfaea69d23
pilar llebres ferriol <pllebres@uoc.edu>
2024-06-17 16:06:58 +0200

    lazy actualidad row

---
+++ dist/index.9f90fba3.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(t){document.getElementById("greetButton").addEventListener("click",function(){alert("¡Hola! Gracias por visitar mi sitio web.")})});
+//# sourceMappingURL=index.9f90fba3.js.map
